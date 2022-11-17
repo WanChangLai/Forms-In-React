7e733a0313e6b03b1a1d41ebd764936319ac91bf
Wan Chang Lai <wanchang.lai@live.com>
2022-11-17 12:47:19 +0800

    Add State and Handler

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import {useState,useRef} from "react";
+
 const SimpleInput = (props) => {
+
+  const nameInputRef = useRef();
+  const[enteredName,setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event =>{
+    setEnteredName(event.target.value )
+  };
+
+  const formSubmissionHandler = event =>{
+    //this to prevent the browser to send a http request to the server once the form is submitted
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+
+    console.log(enteredValue);
+
+    //nameInputRef.current.value = "" => this is not ideal and dont manipulate the DOM
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
